a5a33b44c6ffe8677e07ed14277113d0531450ec
Carson Barnstable <cwbarnstable2.0@gmail.com>
2024-01-09 23:24:24 -0700

    Updating pairingUTRdbCDS.py File

--- V1/Scripts/pairingUTRdbCDS.py
+++ V1/Scripts/pairingUTRdbCDS.py
@@ -1,14 +1,16 @@
 import csv
+
 csv.field_size_limit(600000)
+
 
 def main():
     # record keeping
     utr_rows = []
     cds_rows = []
     full_rows = []
-    CSV_UTR_IN = "Work\\Outputs\\UTRdb_raw_csv_w_ncbi.csv"
-    CSV_CDS_IN = "Work\\Outputs\\records_full_w_stop.csv"
-    CSV_OUT = "Work\\Outputs\\paired_UTRdb_CDS_full_only.csv"
+    CSV_UTR_IN = "..\\Outputs\\UTRdb_raw_csv_w_ncbi.csv"
+    CSV_CDS_IN = "..\\Outputs\\records_full_w_stop.csv"
+    CSV_OUT = "..\\Outputs\\paired_UTRdb_CDS_full_only.csv"
 
     # read in all utr records
     with open(CSV_UTR_IN, 'r') as f:
@@ -21,9 +23,11 @@ def main():
         reader = csv.DictReader(f)
         for row in reader:
             cds_rows.append(row)
-    
+
     # pairwise matching up NCBI IDs
-    full_headers = ["ucsc_id","ncbi_id","ensembl_id","common_name","rpkm","cds_start","cds_stop","aa_len","orf_len","nuc_len","fp_len","tp_len","aa_translation","orf_sequence","nuc_sequence", "five_prime", "three_prime"]
+    full_headers = ["ucsc_id", "ncbi_id", "ensembl_id", "common_name", "rpkm", "cds_start", "cds_stop", "aa_len",
+                    "orf_len", "nuc_len", "fp_len", "tp_len", "aa_translation", "orf_sequence", "nuc_sequence",
+                    "five_prime", "three_prime"]
     for cds_row in cds_rows:
         for utr_row in utr_rows:
             if cds_row["ncbi_id"] == utr_row["ncbi_id"]:
